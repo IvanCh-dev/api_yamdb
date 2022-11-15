beabb331f5d376e4bcfe02e1f7f2aaf2360a8c4a
Алексей <evstigneefff@gmail.com>
2022-11-15 11:17:09 +0300

    “add_user”

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+ROLES = [
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
+]
+
+
+class User(AbstractUser):
+
+    username = models.CharField(
+        db_index=True,
+        max_length=150,
+        unique=True,
+        verbose_name='Логин',
+    )
+    email = models.EmailField(
+        blank=False,
+        max_length=254,
+        unique=True,
+        verbose_name='Почтовый адрес'
+    )
+    first_name = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+        verbose_name='Имя',
+    )
+    last_name = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+        verbose_name='Фамилия',
+    )
+    bio = models.TextField(
+        blank=True,
+        verbose_name='Биография',
+    )
+    role = models.TextField(
+        blank=True,
+        choices=ROLES,
+        default='user',
+        verbose_name='Текущая роль',
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        """Строковое представление модели."""
+        return self.username
